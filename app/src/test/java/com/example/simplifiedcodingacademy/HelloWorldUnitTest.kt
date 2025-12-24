c4039bc799164654017f86e0f6c8001ee8b3de71
ratanakpek <ratanakpek088@gmail.com>
2025-12-24 19:59:07 +0700

    1. Hello World

---
+++ app/src/test/java/com/example/simplifiedcodingacademy/HelloWorldUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.simplifiedcodingacademy
+
+import org.junit.Test
+
+class HelloWorldUnitTest {
+
+    @Test
+    fun makeHelloWorld() {
+        println("Hello World")
+    }
+}
